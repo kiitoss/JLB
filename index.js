47b262862c0d372d7f952ddf3bddb966cc583c7b
Antoine Chataignier <antoinechataignier1998@orange.fr>
2021-05-15 12:36:46 +0200

    refactor: clean code

--- index.js
+++ index.js
@@ -5,20 +5,61 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+class Vote {
+  constructor(id, pseudo, vote) {
+    this.id = id;
+    this.pseudo = pseudo;
+    this.vote_value = vote;
+  }
+}
+
+const list_votes = [];
+
 app.use(express.static(__dirname + '/'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/results.html', (req, res) => {
-  res.sendFile(__dirname + '/results.html');
-});
-
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('new user connected : ' + socket.id);
+  socket.on('new-player', (pseudo) => {
+    list_votes.push(new Vote(socket.id, pseudo, null));
+    io.emit('update-votes', list_votes);
+  });
+
+  socket.on('new-vote', (vote_value) => {
+    list_votes.map(vote => {
+      if (vote.id === socket.id) {
+        vote.vote_value = vote_value;
+        return;
+      }
+    });
+    io.emit('update-votes', list_votes);
+  });
+
+  socket.on('change-pseudo', (new_pseudo) => {
+    list_votes.map(vote => {
+      if (vote.id === socket.id) {
+        vote.pseudo = new_pseudo;
+        return;
+      }
+    });
+    io.emit('update-votes', list_votes);
+  });
+
+  socket.on('start-again', () => {
+    list_votes.map(vote => vote.vote_value = null);
+    io.emit('restart');
+  });
+
   socket.on('disconnect', () => {
-    console.log('user.disconnected');
+    const index = list_votes.findIndex((el) => el.id == socket.id);
+    if (index != -1) {
+      list_votes.splice(index, 1);
+      io.emit('update-votes', list_votes);
+    }
+    console.log('user disconnected : ' + socket.id);
   });
 });
 
--- script.js
+++ script.js
@@ -1,55 +1,142 @@
 const socket = io();
 
 var pseudo;
-const default_pseudo = "anonyme";
-const modal_pseudo = document.getElementById("modal-pseudo");
-const input_pseudo = document.getElementById("input-pseudo");
+
+const DEFAULT_PSEUDO = 'anonyme';
+const MODAL_PSEUDO = document.getElementById('modal-pseudo');
+const INPUT_PSEUDO = document.getElementById('input-pseudo');
+const MODAL_RESULTS = document.getElementById('modal-results');
+const LIST_RESULTS = document.getElementById('list-results');
+const RESULT_VERDICT = document.getElementById('result-verdict');
+const BTN_START_AGAIN = document.getElementById('btn-start-again');
+const BTN_PSEUDO = document.getElementById('btn-pseudo');
+const YES_IMG_PATH = 'res/yes.svg';
+const NO_IMG_PATH = 'res/no.svg';
+const NULL_IMG_PATH = 'res/question-mark.svg';
+const YES_VALUE = 'y';
+const NO_VALUE = 'n';
+const YES_VERDICT = 'YES';
+const NO_VERDICT = 'NO';
+const NULL_VERDICT = 'Egalité';
+const YES_COLOR = '#99ca4e';
+const NO_COLOR = '#fd5e50';
+const NULL_COLOR = '#34568B';
+const BTN_START_COLOR_ACTIVE = '#4b719c';
+const BTN_START_COLOR_NOT_ACTIVE = '#66696e';
+
+function update_verdict(list_votes) {
+  let nb_yes = 0, nb_no = 0,
+      start_again_available = true,
+      verdict, background_color;
+
+  list_votes.map(vote => {
+    if (vote.vote_value === YES_VALUE) nb_yes++;
+    else if (vote.vote_value === NO_VALUE) nb_no++;
+    else start_again_available = false;
+  });
+
+  if (nb_yes > nb_no) {
+    verdict = YES_VERDICT;
+    background_color = YES_COLOR;
+  } else if (nb_no > nb_yes) {
+    verdict = NO_VERDICT;
+    background_color = NO_COLOR;
+  } else {
+    verdict = NULL_VERDICT;
+    background_color = NULL_COLOR;
+  }
+  RESULT_VERDICT.innerHTML = verdict;
+  MODAL_RESULTS.style.backgroundColor = background_color;
+
+  if (start_again_available) {
+    BTN_START_AGAIN.style.backgroundColor = BTN_START_COLOR_ACTIVE;
+    BTN_START_AGAIN.onclick = () => {
+      socket.emit('start-again');
+    }
+  } else {
+    BTN_START_AGAIN.style.backgroundColor = BTN_START_COLOR_NOT_ACTIVE;
+    BTN_START_AGAIN.onclick = () => null;
+  }
+}
+
+function append_vote_to_html(vote) {
+  const vote_html = document.createElement('li');
+
+  let img_path;
+  if (vote.vote_value === YES_VALUE) img_path = YES_IMG_PATH;
+  else if (vote.vote_value === NO_VALUE) img_path = NO_IMG_PATH;
+  else img_path = NULL_IMG_PATH;
+  const img = document.createElement('img');
+  img.setAttribute('src', img_path);
+  img.setAttribute('alt', vote.vote_value + ' image');
+
+  const pseudo = document.createElement('p');
+  pseudo.innerHTML = vote.pseudo;
+
+  vote_html.appendChild(img);
+  vote_html.appendChild(pseudo);
+
+  LIST_RESULTS.appendChild(vote_html);
+}
 
 function update_btn_pseudo() {
-  document.getElementById("btn-pseudo").innerHTML = pseudo;
+  BTN_PSEUDO.innerHTML = pseudo;
 }
+
 function validate_new_pseudo() {
-  let new_pseudo = input_pseudo.value;
-  if (!new_pseudo) new_pseudo = default_pseudo;
-  localStorage.setItem("jlb-pseudo", new_pseudo);
+  let new_pseudo = INPUT_PSEUDO.value;
+  if (!new_pseudo) new_pseudo = DEFAULT_PSEUDO;
+  localStorage.setItem('jlb-pseudo', new_pseudo);
   pseudo = new_pseudo;
+  socket.emit('change-pseudo', new_pseudo);
   update_btn_pseudo();
   close_modal_pseudo();
 }
 
 function btn_yes_clicked() {
-  console.log("Oui");
-  location.href = "./results.html";
+  socket.emit('new-vote', YES_VALUE);
+  MODAL_RESULTS.style.display = 'block';
 }
 
 function btn_no_clicked() {
-  console.log("Non");
+  socket.emit('new-vote', NO_VALUE);
+  MODAL_RESULTS.style.display = 'block';
 }
 
 function close_modal_pseudo() {
-  modal_pseudo.style.display = "none";
+  MODAL_PSEUDO.style.display = 'none';
 }
 
 function modal_pseudo_clicked(event) {
-  if (event.target.id !== "input-pseudo") close_modal_pseudo();
+  if (event.target.id !== 'input-pseudo') close_modal_pseudo();
 }
 
 function open_modal_pseudo() {
-  modal_pseudo.style.display = "block";
-  input_pseudo.placeholder = pseudo;
-  input_pseudo.value = "";
-  input_pseudo.focus();
+  MODAL_PSEUDO.style.display = 'block';
+  INPUT_PSEUDO.placeholder = pseudo;
+  INPUT_PSEUDO.value = '';
+  INPUT_PSEUDO.focus();
 }
 
 function main() {
-  pseudo = localStorage.getItem("jlb-pseudo");
-  if (!pseudo) pseudo = default_pseudo;
+  pseudo = localStorage.getItem('jlb-pseudo');
+  if (!pseudo) pseudo = DEFAULT_PSEUDO;
   update_btn_pseudo();
+  socket.emit('new-player', pseudo);
 }
 
+socket.on('restart', () => MODAL_RESULTS.style.display = 'none');
+
+socket.on('update-votes', (list_votes) => {
+  console.log(list_votes);
+  LIST_RESULTS.innerHTML = '';
+  list_votes.map(vote => append_vote_to_html(vote));
+  update_verdict(list_votes);
+});
+
 window.onload = () => {
-  document.body.style.height = (window.innerHeight * 0.9).toString() + "px";
-  document.body.style.width = (window.innerWidth * 0.9).toString() + "px";
+  document.body.style.height = (window.innerHeight * 0.9).toString() + 'px';
+  document.body.style.width = (window.innerWidth * 0.9).toString() + 'px';
   document.body.style.margin = `${(window.innerHeight * 0.05).toString()}px ${(window.innerWidth * 0.05).toString()}px`;
   main();
 }
